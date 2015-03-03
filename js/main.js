9727171f2d7b6b24f014bad17fe08646669275e3
sydneydowd <sydneydowd@Sydneys-MacBook-Pro.local>
2015-03-03 13:45:49 -0500

    made a var for .form .main-input and .list

---
+++ js/main.js
@@ -0,0 +1,25 @@
+var $form = $('.form');
+var $name = $('.main-input');
+var $list = $('.list');
+
+
+
+$form.on('submit', function (e) {
+    e.preventDefault();
+
+
+    var $li = $('<li>').html($name.val());
+    var $nameDl = $('<button class="delete">X</button>');
+
+    $nameDl.on('click', function () {
+        $li.remove('li');
+    });
+
+   $list.on('click', 'li', function () {
+        $(this).addClass('complete');
+    });
+
+    $li.append($nameDl);
+    $list.prepend($li);
+    $name.val('');
+});
