29aeeca0d00416430209bdc8121bb2cf2aea37ba
sydneydowd <sydneydowd@Sydneys-MacBook-Pro.local>
2015-03-04 12:22:57 -0500

    deleted hover on .complete

--- js/main.js
+++ js/main.js
@@ -2,11 +2,8 @@ var $form = $('.form');
 var $name = $('.main-input');
 var $list = $('.list');
 
-
-
 $form.on('submit', function (e) {
     e.preventDefault();
-
 
     var $li = $('<li>').html($name.val());
     var $nameDl = $('<button class="delete">X</button>');
